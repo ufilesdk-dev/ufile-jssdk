fb2a5a461ef2fb7076c54d6f84e5b15e23bbfbd1
delex.xie <delex.xie@ucloud.cn>
2020-03-05 17:06:33 +0800

    token server 修改put policy签名的bug

--- token_server.php
+++ token_server.php
@@ -47,13 +47,18 @@ class UCloud_Auth {
 
     public function Sign($data, $put_policy)
     {
+        // 上传回调put_policy
+        if ($put_policy) {
+            $policystr = base64_encode(str_replace('"','\\"',json_encode($put_policy)));
+            $singStr = $singStr . ":" . $policystr;
+        }
+            	
         $sign = base64_encode(hash_hmac('sha1', $data, $this->PrivateKey, true));
         $singStr = "UCloud " . $this->PublicKey . ":" . $sign;
 
         // 上传回调put_policy
         if ($put_policy) {
-            $policystr = base64_encode(str_replace('"','\\"',json_encode($put_policy)));
-            $singStr = $singStr . ":" . $policystr;
+            $singStr = $singStr . ":" . $put_policy;
         }
 
         return $singStr;
@@ -68,8 +73,16 @@ class UCloud_Auth {
         $data .=  $content_type . "\n";
         $data .= $date . "\n";
         $data .= CanonicalizedResource($bucket, $key);
+
+        //如果有回调，回调字符串参与计算签名
+        $put_policy_base64 ="";
+        if ($put_policy!="") {
+            $put_policy_base64 = $put_policy;
+            $data .= $put_policy_base64;
+        }
+        
         error_log($data, 3, "/tmp/php.log");
-        return $this->Sign($data, $put_policy);
+        return $this->Sign($data, $put_policy_base64);
     }
 }
 
@@ -80,7 +93,7 @@ $content_md5=$_GET['content_md5'];
 $content_type=$_GET['content_type'];
 $date=$_GET['date'];
 
-$put_policy=$_GET['put_policy'];
+$put_policy=$_GET['put_policy'];	//需要是传已经base64编码后的, 也可以在这个代码里业务固定定制一个
 
 $auth=new UCloud_Auth($UCLOUD_PUBLIC_KEY, $UCLOUD_PRIVATE_KEY);
 printf("%s", $auth->SignRequest($method, $bucket, $key, $content_md5, $content_type, $date, $put_policy));
